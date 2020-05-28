e8e3c262504fef369576d1d2aede7e21d298bf1d
emma-ablitt12 <eablitt@gmail.com>
2020-05-28 14:51:45 +0100

    edit Hand Sanitiser.py

--- handSanitiser.py
+++ handSanitiser.py
@@ -11,22 +11,25 @@ class HandSanitiser:
 				 maximum_capacity,
 				 minimum_capacity,
 				 config_file,
-				 single_dispense
+				 single_dispense,
+				 min_notify_amount
 				 )
 
 	# dispenser id == int, distinguish each dispenser
 	# maximum_capacity == int, in terms of ml, how much the dispenser can hold
-	# minimum_capacity = int, in terms of ml, the lowest the dispenser 
-	#										      can go before refill
+	# minimum_capacity = int, in terms of ml, the lowest the dispenser can go before operation is ceased
 	# config_file == string, containing the path to the json file containing the numbers used
-	# single_dispense == int, in terms of ml, how much is dispensed each time dispenser
-	#																			is used
+	# single_dispense == int, in terms of ml, how much is dispensed each time dispenser is used
+	# min_notify_amount == int, in terms of ml, the minimum amount before user is notified to refill dispenser
+
 
 	#constuctors
 	self.dispenser_id = dispenser_id
 	self.maximum_capacity = maximum_capacity
 	self.minimum_capacity = minimum_capacity
 	self.config_file = config_file
+	self.single_dispense = single_dispense
+	self.min_notify_amount = min_notify_amount
 
 
 	#setters
@@ -35,3 +38,9 @@ class HandSanitiser:
 				print("Programming error.")
 			else:
 				self.single_dispense = quantity
+
+
+	def set_min_notify_amount(self, amount):
+		# the user gets notified when the dispenser is 150ml away from reaching 
+		# the point of operation ceasing
+		amount = self.minimum_capacity + 150
